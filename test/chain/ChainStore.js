cf40837ca1da0c54ba0a4087c557c6484848ec2d
Ruslan Salikhov <ruslan.salihov@gmail.com>
2018-01-27 23:39:19 +0500

    build files

--- test/chain/ChainStore.js
+++ test/chain/ChainStore.js
@@ -1,5 +1,5 @@
 import assert from "assert";
-import {Apis, ChainConfig} from "bitsharesjs-ws";
+import {Apis, ChainConfig} from "travelchainjs-ws";
 import { ChainStore } from "../../lib";
 
 var coreAsset;
--- test/chain/TransactionBuilder.js
+++ test/chain/TransactionBuilder.js
@@ -1,5 +1,5 @@
 import assert from "assert";
-import {Apis} from "bitsharesjs-ws";
+import {Apis} from "travelchainjs-ws";
 import { TransactionBuilder } from "../../lib";
 
 
